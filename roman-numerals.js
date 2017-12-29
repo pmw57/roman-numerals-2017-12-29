dd04171ea97f2221688adfdaa5cd04a3ab04fe52
Paul Wilkins <paul.m.wilkins@gmail.com>
2017-12-29 22:35:53 +1300

    Using correct <= comparison to get roman characters within range of n

--- roman-numerals.js
+++ roman-numerals.js
@@ -2,24 +2,24 @@ module.exports = (function iife() {
     "use strict";
     function convert(n) {
         var CONVERSION_FACTORS = [
-            [4, "IV"],
-            [5, "V"],
+            [10, "X"],
             [9, "IX"],
-            [10, "X"]
+            [5, "V"],
+            [4, "IV"]
         ];
         if (!n || n < 1) {
             return "";
         }
         if (n >= 9) {
             return CONVERSION_FACTORS.find(function ([arabic, ignore]) {
-                return (arabic >= n);
+                return (arabic <= n);
             }).pop();
         }
         if (n === 5) {
-            return CONVERSION_FACTORS[1][1];
+            return CONVERSION_FACTORS[2][1];
         }
         if (n === 4) {
-            return CONVERSION_FACTORS[0][1];
+            return CONVERSION_FACTORS[3][1];
         }
         return convert(n - 1) + "I";
     }
