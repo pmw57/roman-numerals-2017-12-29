52138d7551ab7c0f9c5d78c83c09ed2a1ca70974
Paul Wilkins <paul.m.wilkins@gmail.com>
2017-12-29 19:24:12 +1300

    Add a space after each comma of the results

--- bundle.js
+++ bundle.js
@@ -5,7 +5,7 @@ var romanNumerals = require("./roman-numerals.js");
 var results = document.querySelector(".results");
 var numbers = new Array(100).fill().map((v, i) => i + 1);
 results.innerHTML = numbers.map(romanNumerals.convert)
-    .join(",");
+    .join(", ");
 },{"./roman-numerals.js":2}],2:[function(require,module,exports){
 module.exports = (function iife() {
     "use strict";
--- index.js
+++ index.js
@@ -4,4 +4,4 @@ var romanNumerals = require("./roman-numerals.js");
 var results = document.querySelector(".results");
 var numbers = new Array(100).fill().map((v, i) => i + 1);
 results.innerHTML = numbers.map(romanNumerals.convert)
-    .join(",");
+    .join(", ");
